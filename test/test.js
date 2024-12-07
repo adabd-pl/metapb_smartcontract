6ca10fc216d5c6712770cac155f08bd9d7ba3adb
adabd-pl <72196988+adabd-pl@users.noreply.github.com>
2024-12-07 18:47:12 +0100

    Test update and fixes

--- test/test.js
+++ test/test.js
@@ -1,70 +1,231 @@
-const assert = require('assert');
+const fs = require('fs');
 const sinon = require('sinon');
+const chai = require('chai');
+const { expect } = chai;
 const GraphSmartContract = require('../lib/graphsmartcontract');
 
-describe('updatePermissions', () => {
-  let ctx, graphSmartContract;
-
-  beforeEach(() => {
-    ctx = {
+class GraphTest {
+  constructor() {
+    this.ctx = {
       stub: {
         createCompositeKey: sinon.stub(),
         getState: sinon.stub(),
-        putState: sinon.stub()
-      }
+        putState: sinon.stub(),
+      },
     };
-    graphSmartContract = new GraphSmartContract();
+    this.state = { graph: [] };
+    this.graphSmartContract = new GraphSmartContract();
+
+    this.setupStubs();
+  }
+
+  setupStubs() {
+    this.ctx.stub.createCompositeKey.callsFake((objectType, attributes) => {
+      return `${objectType}:${attributes.join(':')}`;
+    });
+
+    this.ctx.stub.getState.callsFake(async (key) => {
+      const foundItem = this.state.graph.find((item) => `vertex:${item.vertex}` === key);
+      return foundItem ? Buffer.from(JSON.stringify(foundItem)) : null;
+    });
+
+    this.ctx.stub.putState.callsFake(async (key, value) => {
+      const parsedValue = JSON.parse(value.toString());
+      const existing = this.state.graph.find((item) => item.vertex === parsedValue.vertex);
+      if (existing) {
+        Object.assign(existing, parsedValue);
+      } else {
+        this.state.graph.push(parsedValue);
+      }
+    });
+  }
+
+  loadJSON(filePath) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
+  }
+
+  saveJSON(filePath, data) {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  }
+
+
+  getGraphState() {
+    return this.state.graph;
+  }
+}
+
+describe('GraphSmartContract Tests', function () {
+  let graphTest;
+
+  beforeEach(() => {
+    graphTest = new GraphTest();
   });
 
-  it('should throw error for missing required parameters', async () => {
-    try {
-      await graphSmartContract.updatePermissions(ctx, null, 'destinationVertexId', 'permissions', 'operation');
-      assert.fail('Expected error to be thrown');
-    } catch (err) {
-      assert.equal(err.message, 'Missing required parameters');
+  it('should add vertices correctly', async function () {
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, 'U1');
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, 'G1');
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, 'G2');
+    const graphState = graphTest.getGraphState();
+    const vertices = graphState.map((item) => item.vertex);
+    console.log("vertices", vertices);
+    expect(vertices).to.include('U1');
+    expect(vertices).to.include('G1');
+    expect(vertices).to.include('G2');
+  });
+
+  it('should add edges correctly', async function () {
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, 'U1');
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, 'G1');
+
+    await graphTest.graphSmartContract.updatePermissions(
+      graphTest.ctx,
+      'U1',
+      'G1',
+      '101',
+      'ADD'
+    );
+
+    const graphState = graphTest.getGraphState();
+
+    expect(graphState).to.deep.include({
+      vertex: "G1",
+      direct_children_index_table: [
+        {
+          permissions: "101",
+          vertex: "U1"
+        }
+      ],
+      effective_children_index_table: [
+        {
+          permissions: "101",
+          vertex: "U1",
+          intermediaries: [
+            "U1"
+          ]
+        }
+      ],
+      direct_parent_index_table: [],
+      effective_parent_index_table: []
+    });
+    
+  });
+
+
+  it('should calculate effective permissions correctly', async function () {
+
+    const initialGraph = graphTest.loadJSON('graphData.json');
+    graphTest.state.graph = initialGraph;
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, 'U1');
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, 'G1');
+    
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, 'G2');
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, 'A1');
+
+    await graphTest.graphSmartContract.updatePermissions(
+      graphTest.ctx,
+      'U1',
+      'G1',
+      '101',
+      'ADD'
+    );
+
+    await graphTest.graphSmartContract.updatePermissions(
+      graphTest.ctx,
+      'G2',
+      'A1',
+      '001',
+      'ADD'
+    );
+
+    
+    await graphTest.graphSmartContract.updatePermissions(
+      graphTest.ctx,
+      'G1',
+      'G2',
+      '101',
+      'ADD'
+    );
+    const graphState = graphTest.getGraphState();
+    //console.log("graphState", graphState.find((item) => item.vertex == 'A1').effective_children_index_table);
+    expect(graphState).to.deep.include({
+      vertex: "A1",
+      direct_children_index_table: [
+        {
+          permissions: "001",
+          vertex: "G2"
+        }
+      ],
+      effective_children_index_table: [
+        {
+          permissions: "001",
+          vertex: "G2",
+          intermediaries: [
+            "G2"
+          ]
+        },
+        {
+          permissions: "001",
+          vertex: "U1",
+          intermediaries: [
+            "G2"
+          ]
+        },
+        {
+          permissions: "001",
+          vertex: "G1",
+          intermediaries: [
+            "G2"
+          ]
+        },
+      ],
+      direct_parent_index_table: [],
+      effective_parent_index_table: []
+    });
+    
+  });
+
+
+  it('should match the expected graph structure', async function () {
+    const initialGraph = graphTest.loadJSON('graphData.json');
+    graphTest.state.graph = initialGraph;
+
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, "U1" ); 
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, "G1" ); 
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, "G2" ); 
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, "A1" ); 
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, "A2" ); 
+    await graphTest.graphSmartContract.addVertex(graphTest.ctx, "G3" );;
+    await graphTest.graphSmartContract.updatePermissions(graphTest.ctx, 'U1', 'G1', '010', 'ADD');
+    await graphTest.graphSmartContract.updatePermissions(graphTest.ctx, 'G1', 'A1', '100', 'ADD');
+    await graphTest.graphSmartContract.updatePermissions(graphTest.ctx, 'G2', 'A1', '001', 'ADD');
+    await graphTest.graphSmartContract.updatePermissions(graphTest.ctx, 'G3', 'A2', '101', 'ADD');
+    await graphTest.graphSmartContract.updatePermissions(graphTest.ctx, 'G2', 'G3', '011', 'ADD');
+    await graphTest.graphSmartContract.updatePermissions(graphTest.ctx, 'U1', 'G2', '100', 'ADD');
+
+    const finalGraph = sortJsonArrays(graphTest.getGraphState());
+    const expectedGraph = sortJsonArrays(graphTest.loadJSON('expectedGraph.json'));
+    graphTest.saveJSON('outputGraph.json', finalGraph);
+
+    expect(finalGraph).to.deep.equal(expectedGraph);
+  });
+
+
+  function sortJsonArrays(obj) {
+    if (Array.isArray(obj)) {
+        return obj
+            .map(sortJsonArrays) 
+            .sort((a, b) => JSON.stringify(a).localeCompare(JSON.stringify(b)));
+    } else if (obj && typeof obj === 'object') {
+        return Object.keys(obj)
+            .sort()
+            .reduce((acc, key) => {
+                acc[key] = sortJsonArrays(obj[key]);
+                return acc;
+            }, {});
     }
-  });
+    return obj;
+}
 
-  it('should throw error for non-existent source vertex', async () => {
-    ctx.stub.getState.resolves(null);
-    try {
-      await graphSmartContract.updatePermissions(ctx, 'sourceVertexId', 'destinationVertexId', 'permissions', 'operation');
-      assert.fail('Expected error to be thrown');
-    } catch (err) {
-      assert.equal(err.message, 'sourceVertexId does not exist');
-    }
-  });
-
-  it('should throw error for non-existent destination vertex', async () => {
-    ctx.stub.getState.onCall(0).resolves('sourceVertex');
-    ctx.stub.getState.onCall(1).resolves(null);
-    try {
-      await graphSmartContract.updatePermissions(ctx, 'sourceVertexId', 'destinationVertexId', 'permissions', 'operation');
-      assert.fail('Expected error to be thrown');
-    } catch (err) {
-      assert.equal(err.message, 'destinationVertexId does not exist');
-    }
-  });
-
-  it('should successfully update permissions', async () => {
-    const sourceVertexJson = { effective_children_index_table: [] };
-    const destinationVertexJson = { effective_parent_index_table: [] };
-    ctx.stub.getState.onCall(0).resolves(Buffer.from(JSON.stringify(sourceVertexJson)));
-    ctx.stub.getState.onCall(1).resolves(Buffer.from(JSON.stringify(destinationVertexJson)));
-    console.log(sourceVertexJson)
-    await graphSmartContract.updatePermissions(ctx, 'sourceVertexId', 'destinationVertexId', 'permissions', 'operation');
-    assert(ctx.stub.putState.calledTwice);
-  });
-
-  it('should handle error during permission update', async () => {
-    ctx.stub.getState.onCall(0).resolves('sourceVertex');
-    ctx.stub.getState.onCall(1).resolves('destinationVertex');
-    sinon.stub(graphSmartContract, 'updatePermissionsHelper').rejects(new Error('Test error'));
-    try {
-      await graphSmartContract.updatePermissions(ctx, 'sourceVertexId', 'destinationVertexId', 'permissions', 'operation');
-      assert.fail('Expected error to be thrown');
-    } catch (err) {
-      assert.equal(err.message, 'Failed to update permissions');
-    }
-  });
+ 
 });
--- visualization/client.js
+++ visualization/client.js
@@ -29,36 +29,30 @@ async function simulateGraphOperations(inputFilePath, outputFilePath) {
   state.graph = initialGraph;
 
   ctx.stub.createCompositeKey = sinon.stub().callsFake((objectType, attributes) => {
-    // Zakładamy, że objectType jest nazwą obiektu (np. 'vertex'), a attributes to tablica atrybutów.
-    // Złączamy 'objectType' i 'attributes' (np. destinationVertexId) w jeden klucz złożony.
     return `${objectType}:${attributes.join(':')}`;
   });
   
 
   ctx.stub.getState = sinon.stub().callsFake(async (key) => {
-    // Znajdź obiekt w state.graph na podstawie klucza 'key' (np. vertex).
     const foundItem = state.graph.find(item => ('vertex:' + item.vertex) == key);
   
-    // Jeśli obiekt został znaleziony, zwróć go w postaci Buffer, w przeciwnym razie zwróć null.
     return foundItem ? Buffer.from(JSON.stringify(foundItem)) : null;
   });
   
 
   ctx.stub.putState = sinon.stub().callsFake(async (key, value) => {
-    // Jeśli 'state.graph' nie jest tablicą, to inicjujemy ją jako pustą tablicę.
+    console.log(`Saving key: ${key}, value: ${value.toString()}`);
     if (!Array.isArray(state.graph)) {
       state.graph = [];
     }
     
-    // Tworzymy nowy obiekt na podstawie wartości (parsowanie JSON) i dodajemy do tablicy.
     const parsedValue = JSON.parse(value.toString());
-  
-    // Jeśli klucz nie istnieje, dodajemy nowy obiekt, jeśli istnieje - nadpisujemy.
-    let existing = state.graph.find(item => item.vertex === parsedValue.vertex);
+    console.log(parsedValue)
+    let existing = state.graph.find(item => item.vertex == parsedValue.vertex);
     if (existing) {
-      Object.assign(existing, parsedValue); // Nadpisujemy istniejący obiekt, jeżeli jest
+      Object.assign(existing, parsedValue); 
     } else {
-      state.graph.push(parsedValue); // Dodajemy nowy obiekt do tablicy
+      state.graph.push(parsedValue);
     }
   });
   
@@ -68,17 +62,25 @@ async function simulateGraphOperations(inputFilePath, outputFilePath) {
   await graphSmartContract.addVertex(ctx, "G2" ); 
   await graphSmartContract.addVertex(ctx, "A1" ); 
   await graphSmartContract.addVertex(ctx, "A2" ); 
-  await graphSmartContract.addVertex(ctx, "A3" );
+  await graphSmartContract.addVertex(ctx, "G3" );
 
   console.log("Actual graph state: ",state.graph);
 
    // Add Edges
-  await graphSmartContract.updatePermissions(ctx, "U1" ,  "G1" , "101" ,"ADD"); 
-  await graphSmartContract.updatePermissions(ctx, "U1" ,  "A1" , "101" ,"ADD");
-  await graphSmartContract.updatePermissions(ctx, "G1" ,  "A1" , "010" ,"ADD"); 
-  await graphSmartContract.updatePermissions(ctx, "G1" ,  "G2" , "110" ,"ADD");
-  await graphSmartContract.updatePermissions(ctx, "G2" ,  "A2" , "011" ,"ADD"); 
-  await graphSmartContract.updatePermissions(ctx, "G2" ,  "A3" , "001" ,"ADD"); 
+  // await graphSmartContract.updatePermissions(ctx, "U1" ,  "G1" , "101" ,"ADD"); 
+  // await graphSmartContract.updatePermissions(ctx, "U1" ,  "A1" , "101" ,"ADD");
+  // await graphSmartContract.updatePermissions(ctx, "G1" ,  "A1" , "010" ,"ADD"); 
+  // await graphSmartContract.updatePermissions(ctx, "G1" ,  "G2" , "110" ,"ADD");
+  // await graphSmartContract.updatePermissions(ctx, "G2" ,  "A2" , "011" ,"ADD"); 
+  // await graphSmartContract.updatePermissions(ctx, "G2" ,  "A3" , "001" ,"ADD"); 
+
+  let operation = "ADD";
+  await graphSmartContract.updatePermissions(ctx, 'U1', 'G1', '010', operation);
+  await graphSmartContract.updatePermissions(ctx, 'G1', 'A1', '101', operation);
+  await graphSmartContract.updatePermissions(ctx, 'G2', 'A1', '001', operation);
+  await graphSmartContract.updatePermissions(ctx, 'G3', 'A2', '101', operation);
+  await graphSmartContract.updatePermissions(ctx, 'G2', 'G3', '011', operation);
+  await graphSmartContract.updatePermissions(ctx, 'U1', 'A1', '100', operation);
 
   const finalGraph = state.graph;
   saveJSON(outputFilePath, finalGraph);
